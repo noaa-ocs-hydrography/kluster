56e8e38ddc89475c54a8380c25042b144726fab0
eyou102 <eyou102@gmail.com>
2020-10-22 10:27:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-            'hstb.drivers'
+            'hstb.drivers',
             'dask>=2.17.2', 
             'fasteners',
             'laspy>=1.7.0',
