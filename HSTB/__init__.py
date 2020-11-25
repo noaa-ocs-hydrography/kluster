a4e6bcb2ad8d47b5ba51de0cb6ab0e348dd5903f
eyou102 <eyou102@gmail.com>
2020-11-25 09:59:41 -0500

    include environment sitepackages in HSTB path

--- HSTB/__init__.py
+++ HSTB/__init__.py
@@ -1,7 +1,5 @@
 import os
+import numpy
 normal_path = os.path.dirname(__file__)
-# os.path.normpath(os.path.join(normal_path, "../../Python2/HSTB"))
-# make this work in either the testing or final environment
-old_path = os.path.normpath(normal_path.replace("Python38_test", "Python2", 1))
-old_path = os.path.normpath(normal_path.replace("Python38", "Python2", 1))
-__path__ = [normal_path, old_path]
+install_path = os.path.join(os.path.dirname(numpy.__path__[0]), 'HSTB')
+__path__ = [normal_path, install_path]
