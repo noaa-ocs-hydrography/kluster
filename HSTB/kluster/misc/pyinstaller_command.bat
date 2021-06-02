2fa4eda3832a37a07c7a5df78fd5d003413c6593
eyou102 <eyou102@gmail.com>
2021-06-02 10:45:02 -0400

    add pyinstaller helpers, modify the spec file to work in place

---
+++ HSTB/kluster/misc/pyinstaller_command.bat
@@ -0,0 +1,1 @@
+cd C:\Pydro21_Dev\NOAA\site-packages\Python38\git_repos\hstb_kluster\HSTB\kluster\misc && C:\Pydro21_Dev\Scripts\activate Pydro38 && pyinstaller "C:\Pydro21_Dev\NOAA\site-packages\Python38\git_repos\hstb_kluster\HSTB\kluster\misc\kluster_main.spec"
