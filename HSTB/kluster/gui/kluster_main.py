8be458e1c9e975e1bf15c7609ba4926630e79c1c
eyou102 <eyou102@gmail.com>
2021-07-30 15:23:43 -0400

    resolve bug with showing vr surfaces, load all resolutions

--- HSTB/kluster/gui/kluster_main.py
+++ HSTB/kluster/gui/kluster_main.py
@@ -389,11 +389,14 @@ class KlusterMain(QtWidgets.QMainWindow):
                     self.two_d.remove_surface(remove_surface, resolution)
         if add_surface is not None and surface_layer_name:
             surf_object = self.project.surface_instances[add_surface]
+            needs_drawing = []
             for resolution in surf_object.resolutions:
                 shown = self.two_d.show_surface(add_surface, surface_layer_name, resolution)
                 if not shown:  # show didnt work, must need to add the surface instead, loading from disk...
-                    self.draw_surface_thread.populate(add_surface, surf_object, resolution, surface_layer_name)
-                    self.draw_surface_thread.start()
+                    needs_drawing.append(resolution)
+            if needs_drawing:
+                self.draw_surface_thread.populate(add_surface, surf_object, needs_drawing, surface_layer_name)
+                self.draw_surface_thread.start()
 
         if new_fqprs is not None and new_fqprs:
             self.draw_navigation_thread.populate(self.project, new_fqprs)
@@ -1131,12 +1134,16 @@ class KlusterMain(QtWidgets.QMainWindow):
         if not self.draw_surface_thread.error:
             surf_path = self.draw_surface_thread.surface_path
             surf_epsg = self.draw_surface_thread.surf_object.epsg
-            surf_resolution = self.draw_surface_thread.resolution
-            for surflayername in self.draw_surface_thread.surface_data:
-                data = self.draw_surface_thread.surface_data[surflayername][0]
-                geo_transform = self.draw_surface_thread.surface_data[surflayername][1]
-                self.two_d.add_surface([surf_path, surflayername, data, geo_transform, surf_epsg, surf_resolution])
-            self.two_d.set_extents_from_surfaces(surf_path, surf_resolution)
+            drawresolution = None
+            for surf_resolution in self.draw_surface_thread.surface_data:
+                for surflayername in self.draw_surface_thread.surface_data[surf_resolution]:
+                    data = self.draw_surface_thread.surface_data[surf_resolution][surflayername][0]
+                    geo_transform = self.draw_surface_thread.surface_data[surf_resolution][surflayername][1]
+                    self.two_d.add_surface([surf_path, surflayername, data, geo_transform, surf_epsg, surf_resolution])
+                    if not drawresolution:
+                        drawresolution = surf_resolution
+            if drawresolution:
+                self.two_d.set_extents_from_surfaces(surf_path, drawresolution)
         self.draw_surface_thread.populate(None, None, None, None)
         self._stop_action_progress()
         print('draw_surface: Drawing surface complete.')
