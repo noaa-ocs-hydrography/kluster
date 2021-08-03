4331a48bc01ac619010a1c02c6c5654d4354278b
eyou102 <eyou102@gmail.com>
2021-08-03 11:40:24 -0400

    Update script_tests.py

--- HSTB/kluster/script_tests.py
+++ HSTB/kluster/script_tests.py
@@ -104,24 +104,6 @@ roll = dg['sample']['KMdefault']['roll_deg']
 km.closeFile()
 plt.plot(tme)
 
-############################## accuracy tests ###################################
-# from fqpr_sat import accuracy_test
-# output_directory = r"C:\collab\dasktest\data_dir\outputtest\acc_test_screengrabs"
-# reference_surface = r"C:\collab\dasktest\data_dir\outputtest\hassler_refsurf.npz"
-# linepairs = [r"C:\collab\dasktest\data_dir\outputtest\hassler_400_long_fm",
-#              r"C:\collab\dasktest\data_dir\outputtest\hassler_400_short_fm",
-#              r"C:\collab\dasktest\data_dir\outputtest\hassler_400_long_cw",
-#              r"C:\collab\dasktest\data_dir\outputtest\hassler_400_short_cw",
-#              r"C:\collab\dasktest\data_dir\outputtest\hassler_300_long_fm",
-#              r"C:\collab\dasktest\data_dir\outputtest\hassler_300_short_fm",
-#              r"C:\collab\dasktest\data_dir\outputtest\hassler_300_long_cw",
-#              r"C:\collab\dasktest\data_dir\outputtest\hassler_300_short_cw",
-#              r"C:\collab\dasktest\data_dir\outputtest\hassler_200_long_fm",
-#              r"C:\collab\dasktest\data_dir\outputtest\hassler_200_short_fm",
-#              r"C:\collab\dasktest\data_dir\outputtest\hassler_200_long_cw",
-#              r"C:\collab\dasktest\data_dir\outputtest\hassler_200_short_cw"]
-# accuracy_test(reference_surface, linepairs, vert_ref='waterline', output_directory=output_directory)
-
 ###################################### iterate through finding the right offset ###############################
 
 from fqpr_generation import *
@@ -168,93 +150,6 @@ st.add_file(r"C:\collab\dasktest\data_dir\tj_patch_test\S222_PatchTest_DN077\Pro
 st.add_file(r"C:\collab\dasktest\data_dir\tj_patch_test\S222_PatchTest_DN077\Processed\SBET\S222_2020_KongsbergEM2040\2020-077\S222_2020_KongsbergEM2040_2020-077_NAD83_smrmsg.out")
 
 st.start_folder_monitor(r'C:\collab\dasktest\data_dir\tj_patch_test', is_recursive=True)
-
-########################################## working on sv 2 #####################################
-
-from fqpr_generation import *
-from fqpr_convenience import *
-
-fq = reload_data(r"C:\collab\dasktest\data_dir\outputtest\shipname")
-self = fq
-subset_time = None
-
-systems = self.multibeam.return_system_time_indexed_array(subset_time=subset_time)
-for s_cnt, system in enumerate(systems):
-    ra = self.multibeam.raw_ping[s_cnt]
-    sys_ident = ra.system_identifier
-    self.logger.info('Operating on system serial number = {}'.format(sys_ident))
-    self.initialize_intermediate_data(sys_ident, 'sv_corr')
-    pings_per_chunk, max_chunks_at_a_time = self.get_cluster_params()
-    profnames, casts, cast_times, castlocations = self.multibeam.return_all_profiles()
-
-    for applicable_index, timestmp, prefixes in system:
-        self.logger.info('using installation params {}'.format(timestmp))
-        idx_by_chunk = self.return_chunk_indices(applicable_index, pings_per_chunk)
-        if len(idx_by_chunk[0]):  # if there are pings in this sector that align with this installation parameter record
-            cast_chunks = self.return_cast_idx_nearestintime(cast_times, idx_by_chunk)
-            addtl_offsets = self.return_additional_xyz_offsets(ra, prefixes, timestmp, idx_by_chunk)
-            data_for_workers = self._generate_chunks_svcorr(ra, casts, cast_chunks, applicable_index, prefixes, timestmp, addtl_offsets)
-            break
-    break
-
-cast, beam_azimuth, beam_angle, two_way_travel_time, surface_sound_speed, z_waterline_offset, additional_offsets = data_for_workers[0][0].result(), data_for_workers[0][1].result()[0], data_for_workers[0][1].result()[1], data_for_workers[0][2].result(), data_for_workers[0][3].result(), data_for_workers[0][4], data_for_workers[0][5].result()
-
-######################################################################################################
-from fqpr_surface_v3 import *
-from time import perf_counter
-from HSTB.kluster.fqpr_convenience import *
-
-fq = reload_data(r"C:\Users\eyou1\Downloads\em2040_40224_02_15_2021", skip_dask=True)
-
-# x = np.random.uniform(538900, 539300, 1000).astype(np.float32)
-# y = np.random.uniform(5292800, 5293300, 1000).astype(np.float32)
-# z = np.random.uniform(30, 35, 1000).astype(np.float32)
-#
-# test_data_arr = np.stack([x, y, z], axis=1)
-# test_data_arr = test_data_arr.ravel().view(dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4')])
-
-raw_ping = fq.multibeam.raw_ping[0]
-dataset = raw_ping.drop_vars([nms for nms in raw_ping.variables if nms not in ['x', 'y', 'z', 'tvu', 'thu']])
-dataset = dataset.isel(time=slice(0, 3600)).stack({'sounding': ('time', 'beam')})
-fq.close()
-
-allowed_variables = ['x', 'y', 'z', 'tvu']
-dtype = [(varname, dataset[varname].dtype) for varname in allowed_variables if varname in dataset]
-data_arr = np.empty(len(dataset['x']), dtype=dtype)
-for nm, typ in dtype:
-    data_arr[nm] = dataset[nm].values
-data_dask = da.from_array(data_arr)
-
-
-def timethisthing(userfunc, args, kwargs, msg):
-    st = perf_counter()
-    ret = userfunc(*args, **kwargs)
-    end = perf_counter()
-    print(msg.format(end-st))
-    return ret
-
-# qm = QuadManager()
-# timethisthing(qm.create, [data_arr], {'max_points_per_quad': 5}, 'Numpy build time: {}')
-# timethisthing(qm.save, [r'C:\collab\dasktest\data_dir\outputtest\tj_patch_test_2040'], {}, 'Numpy save time: {}')
-# timethisthing(qm.tree.draw_tree, [], {'plot_points': False}, 'Numpy draw time: {}')
-#
-# qm = QuadManager()
-# timethisthing(qm.create, [data_dask], {'max_points_per_quad': 5}, 'Dask build time: {}')
-# timethisthing(qm.save, [r'C:\collab\dasktest\data_dir\outputtest\tj_patch_test_2040'], {}, 'Dask save time: {}')
-# timethisthing(qm.tree.draw_tree, [], {'plot_points': False}, 'Dask draw time: {}')
-
-qm = QuadManager()
-coordsys = dataset.horizontal_crs
-vertref = dataset.vertical_reference
-containername = os.path.split(dataset.output_path)[1]
-multibeamlist = list(dataset.multibeam_files.keys())
-
-timethisthing(qm.create, [dataset],
-              {'container_name': containername, 'multibeam_file_list': multibeamlist,
-               'crs': coordsys, 'vertical_reference': vertref, 'min_grid_size': 1, 'max_grid_size': 1},
-              'Dataset build time: {}')
-qm.plot_surface('depth')
-qm.tree.draw_tree()
 
 ####################################################################################
 from HSTB.kluster.fqpr_convenience import *
@@ -317,45 +212,6 @@ for cnt, d in enumerate(data):
 if driver == 'GTiff':  # gtiff driver wants one no data value for all bands
     dataset.GetRasterBand(1).SetNoDataValue(nodatavalue)
 dataset = None
-
-############################################################################################
-from HSTB.kluster.fqpr_convenience import *
-from HSTB.kluster.gdal_helpers import *
-from HSTB.kluster.fqpr_surface_v3 import *
-
-fq = reload_data(r'C:\collab\dasktest\data_dir\outputtest\tj_patch_test_710')
-
-# {'0000_20200317_062845_S222_EM710.all': [1584426525.807, 1584426639.304],
-#  '0001_20200317_063913_S222_EM710.all': [1584427153.265, 1584427342.72],
-#  '0002_20200317_064945_S222_EM710.all': [1584427785.168, 1584427891.757],
-#  '0003_20200317_065753_S222_EM710.all': [1584428273.944, 1584428468.645],
-#  '0004_20200317_071004_S222_EM710.all': [1584429004.248, 1584429117.759],
-#  '0005_20200317_072016_S222_EM710.all': [1584429616.899, 1584429791.078],
-#  '0006_20200317_074548_S222_EM710.all': [1584431148.983, 1584431279.806],
-#  '0007_20200317_075822_S222_EM710.all': [1584431902.919, 1584432001.206],
-#  '0008_20200317_081048_S222_EM710.all': [1584432648.714, 1584432748.339],
-#  '0009_20200317_082551_S222_EM710.all': [1584433551.414, 1584433666.558],
-#  '0010_20200317_083654_S222_EM710.all': [1584434214.578, 1584434297.872],
-#  '0011_20200317_084855_S222_EM710.all': [1584434935.098, 1584435041.788],
-#  '0012_20200317_090126_S222_EM710.all': [1584435686.38, 1584435772.905],
-#  '0013_20200317_091555_S222_EM710.all': [1584436555.863, 1584436678.324],
-#  '0014_20200317_093221_S222_EM710.all': [1584437541.661, 1584437624.045],
-#  '0015_20200317_094852_S222_EM710.all': [1584438532.577, 1584438683.563]}
-
-for cnt, tim in enumerate([1584426639.304, 1584427342.72, 1584427891.757, 1584428468.645, 1584429117.759, 1584429791.078,
-                           1584431279.806, 1584432001.206, 1584432748.339, 1584433666.558, 1584434297.872, 1584435041.788,
-                           1584435772.905, 1584436678.324, 1584437624.045, 1584438683.563]):
-    fq.subset_by_time(1584426525.807, tim)
-    surf = generate_new_surface(fq, min_grid_size=1, max_grid_size=1)
-    plt.imshow(surf.node_data['z'])
-    plt.title('{}-{}'.format(1584426525.807, tim))
-    plt.savefig(r'C:\collab\dasktest\data_dir\outputtest\tj_patch_test_710\fig{}.tif'.format(cnt))
-    plt.close()
-    fq.restore_subset()
-
-
-fq.subset_by_time(1584426525.807, 1584429117.759)
-surf = generate_new_surface(fq, min_grid_size=1, max_grid_size=1)
 
 #########################################################################################
 # gdal_translate "C:\vdatum_all_20201203\vdatum\FLandrew02_8301\mllw.gtx" "C:\vdatum_all_20201203\vdatum\chart_datum_depth.tif"
