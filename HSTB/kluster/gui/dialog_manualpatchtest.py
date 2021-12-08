82ca45c83680ee75d3d3361903ee6059cf827d94
eyou102 <eyou102@gmail.com>
2021-12-08 16:52:47 -0500

    bug fix with populating patch test dialog

--- HSTB/kluster/gui/dialog_manualpatchtest.py
+++ HSTB/kluster/gui/dialog_manualpatchtest.py
@@ -26,21 +26,21 @@ class PrePatchDialog(QtWidgets.QDialog):
         self.xyzrph_list = XyzrphList(self)
         self.main_layout.addWidget(self.xyzrph_list)
 
+        self.hlayout_msg = QtWidgets.QHBoxLayout()
+        self.warning_message = QtWidgets.QLabel('', self)
+        self.warning_message.setStyleSheet("{};".format(kluster_variables.error_color))
+        self.hlayout_msg.addWidget(self.warning_message)
+        self.main_layout.addLayout(self.hlayout_msg)
+
         self.button_layout = QtWidgets.QHBoxLayout()
         self.button_layout.addStretch(1)
-        self.ok_button = QtWidgets.QPushButton('Run', self)
+        self.ok_button = QtWidgets.QPushButton('Load', self)
         self.button_layout.addWidget(self.ok_button)
         self.button_layout.addStretch(1)
         self.cancel_button = QtWidgets.QPushButton('Cancel', self)
         self.button_layout.addWidget(self.cancel_button)
         self.button_layout.addStretch(1)
         self.main_layout.addLayout(self.button_layout)
-
-        self.hlayout_msg = QtWidgets.QHBoxLayout()
-        self.warning_message = QtWidgets.QLabel('', self)
-        self.warning_message.setStyleSheet("{};".format(kluster_variables.error_color))
-        self.hlayout_msg.addWidget(self.warning_message)
-        self.main_layout.addLayout(self.hlayout_msg)
 
         self.setLayout(self.main_layout)
         self.canceled = False
@@ -125,17 +125,17 @@ class PrePatchDialog(QtWidgets.QDialog):
             for i in range(len(selected_rows) - 1):
                 next_sel = selected_rows[i + 1]
                 if self.roll[base_sel_row] != self.roll[next_sel]:
-                    msg = 'Roll does not match between rows {} and {}!'.format(base_sel_row, next_sel)
+                    msg = 'Roll does not match between rows {} and {}!'.format(base_sel_row + 1, next_sel + 1)
                 elif self.pitch[base_sel_row] != self.pitch[next_sel]:
-                    msg = 'Pitch does not match between rows {} and {}!'.format(base_sel_row, next_sel)
+                    msg = 'Pitch does not match between rows {} and {}!'.format(base_sel_row + 1, next_sel + 1)
                 elif self.heading[base_sel_row] != self.heading[next_sel]:
-                    msg = 'Heading does not match between rows {} and {}!'.format(base_sel_row, next_sel)
+                    msg = 'Heading does not match between rows {} and {}!'.format(base_sel_row + 1, next_sel + 1)
                 elif self.latency[base_sel_row] != self.latency[next_sel]:
-                    msg = 'Latency does not match between rows {} and {}!'.format(base_sel_row, next_sel)
+                    msg = 'Latency does not match between rows {} and {}!'.format(base_sel_row + 1, next_sel + 1)
                 elif self.serial_numbers[base_sel_row] != self.serial_numbers[next_sel]:
-                    msg = 'Serial Number does not match between rows {} and {}!'.format(base_sel_row, next_sel)
+                    msg = 'Serial Number does not match between rows {} and {}!'.format(base_sel_row + 1, next_sel + 1)
                 elif self.headindexes[base_sel_row] != self.headindexes[next_sel]:
-                    msg = 'Sonar Head does not match between rows {} and {}!'.format(base_sel_row, next_sel)
+                    msg = 'Sonar Head does not match between rows {} and {}!'.format(base_sel_row + 1, next_sel + 1)
         if msg:
             err = True
         return selected_rows, err, msg
