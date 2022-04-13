allow for exporting grids with the correct sign convention

HSTB/kluster/gui/kluster_worker.py
@@ -179,7 +179,7 @@ class DrawSurfaceWorker(QtCore.QThread):
                     self.surface_data[resolution] = {}
                     chunk_count = 1
                     for geo_transform, maxdim, data in self.surf_object.get_chunks_of_tiles(resolution=resolution, layer=surface_layer_name,
-                                                                                            nodatavalue=np.float32(np.nan), z_positive_up=False,
+                                                                                            nodatavalue=np.float32(np.nan), z_positive_up=self.surf_object.positive_up,
                                                                                             for_gdal=True):
                         data = list(data.values())
                         self.surface_data[resolution][self.surface_layer_name + '_{}'.format(chunk_count)] = [data, geo_transform]