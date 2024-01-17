8d7927db98404edafa39509ba47eb6fa64b6b440
Barry Gallagher <barry.gallagher@noaa.gov>
2024-01-16 19:57:04 -0500

    remove duplicate line, fix tabs/spaces

--- setup.py
+++ setup.py
@@ -43,10 +43,9 @@ REQUIRED = [
             'watchdog>=2.1.6',
             'xarray==0.20.2',
             'zarr==2.10.3',
-            'hstb.resources @ git+https://github.com/noaa-ocs-hydrography/hstb_resources.git#egg=hstb.resources',
             'hstb.drivers @ git+https://github.com/noaa-ocs-hydrography/drivers.git#egg=hstb.drivers',
             'hstb.shared @ git+https://github.com/noaa-ocs-hydrography/shared.git#egg=hstb.shared',
-			'hstb.resources @ git+https://github.com/noaa-ocs-hydrography/hstb_resources.git#egg=hstb.resources',
+            'hstb.resources @ git+https://github.com/noaa-ocs-hydrography/hstb_resources.git#egg=hstb.resources',
             'vyperdatum @ git+https://github.com/noaa-ocs-hydrography/vyperdatum.git#egg=vyperdatum',
             'bathygrid @ git+https://github.com/noaa-ocs-hydrography/bathygrid.git#egg=bathygrid'
 
