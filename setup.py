d6e073baae18fe986edbe562b22b8e06fdd646a0
Brian Miles <selimnairb@gmail.com>
2024-03-12 11:35:22 -0400

    Roll-back HSTB dependency URLs to official URLs to facilitate kluster repo PR

--- setup.py
+++ setup.py
@@ -43,11 +43,11 @@ REQUIRED = [
             'watchdog~=2.3.1',
             'xarray~=2024.2.0',
             'zarr~=2.13.6',
-            'hstb.drivers @ git+https://github.com/selimnairb/HSTB-drivers.git#egg=hstb.drivers',
-            'hstb.shared @ git+https://github.com/selimnairb/HSTB-shared.git#egg=hstb.shared',
-			'hstb.resources @ git+https://github.com/noaa-ocs-hydrography/hstb_resources.git#egg=hstb.resources',
-            'vyperdatum @ git+https://github.com/selimnairb/vyperdatum.git#egg=vyperdatum',
-            'bathygrid @ git+https://github.com/selimnairb/bathygrid.git#egg=bathygrid'
+            'hstb.drivers @ git+https://github.com/noaa-ocs-hydrography/drivers.git#egg=hstb.drivers',
+            'hstb.shared @ git+https://github.com/noaa-ocs-hydrography/shared.git#egg=hstb.shared',
+            'hstb.resources @ git+https://github.com/noaa-ocs-hydrography/hstb_resources.git#egg=hstb.resources',
+            'vyperdatum @ git+https://github.com/noaa-ocs-hydrography/vyperdatum.git#egg=vyperdatu',
+            'bathygrid @ git+https://github.com/noaa-ocs-hydrography/bathygrid.git#egg=bathygrid'
 
             # note to self about gdal.  I want gdal >= 3.2.3 to get the bag template fix, but qgis appears to only
             #  support gdal=3.2.2 in it's latest version.  So I need to wait on qgis to get the gdal fix.n
