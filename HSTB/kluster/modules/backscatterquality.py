a7478ddd4138a58e64d3f41e167f81b6cf85867f
Harper Umfress <harper.umfress@gmail.com>
2024-05-18 09:42:22 -0700

    Updates to beam pattern calibration capability. Still in progress, but provides calibration values in a dict when given a directory of data.

--- HSTB/kluster/modules/backscatterquality.py
+++ HSTB/kluster/modules/backscatterquality.py
@@ -197,11 +197,11 @@ def create_raw_bs_evaluation(fle_dir):
             ax[1].plot([sector_border, sector_border], [0, reflectivity.shape[0]], 'y', label=plt_label)
             plt_label = "_nolegend_"
 
-        cmap = plt.get_cmap('rainbow')
-        cmap.set_over('blue')
+        cmap_rainbow = plt.get_cmap('rainbow')
+        cmap_rainbow.set_over('blue')
         # bad_beam_arr = np.nan_to_num(bad_beam_arr)
         # bad_beam_arr[bad_beam_arr == 2] = np.nan
-        ax[1].imshow(bad_beam_arr, aspect='auto', interpolation='none', vmin=0, vmax=1, cmap=cmap, label='Bad Soundings')
+        ax[1].imshow(bad_beam_arr, aspect='auto', interpolation='none', vmin=0, vmax=1, cmap=cmap_rainbow, label='Bad Soundings')
         handles, labels = ax[1].get_legend_handles_labels()
         point = Line2D([0], [0], label='Bad Soundings', marker='s', markersize=10, markerfacecolor='blue', linestyle='')
         handles.extend([point])
@@ -273,11 +273,18 @@ def prepare_backscatter_for_file(fle):
     # data_dict[fle] = [{'mode': mode}, {'data': data}]
     approx_sv_mean = 1500
     reflectivity2 = data['ping']['reflectivity']
-    reflectivity = reflectivity2
+    # reflectivity = reflectivity2
 
     #Adding Hacky Reflectivity1 Reader in here
     km = kmall.kmall(fle)
     reflectivity1 = []
+    bs_oblique = []
+    bs_normal = []
+    seabedimage_snippets = []
+    seabedimage_start = []
+    seabedimage_numsamples = []
+    seabedimage_centersample = []
+
     while not km.eof:
         km.decode_datagram()
         if km.datagram_ident != 'MRZ':
@@ -285,12 +292,41 @@ def prepare_backscatter_for_file(fle):
         else:
             km.read_datagram()
             ref1_ping = km.datagram_data['sounding']['reflectivity1_dB']
+            bs_oblique_ping = km.datagram_data['rxInfo']['BSoblique_dB']
+            bs_normal_ping = km.datagram_data['rxInfo']['BSnormal_dB']
+            seabedimage_snippets_ping = km.datagram_data['SIsample_desidB'] #snippets
+            seabedimage_start_ping = km.datagram_data['sounding']['SIstartRange_samples']
+            seabedimage_numsamples_ping = km.datagram_data['sounding']['SInumSamples']
+            seabedimage_centersample_ping = km.datagram_data['sounding']['SIcentreSample']
+
             reflectivity1.append(ref1_ping)
+            bs_oblique.append(bs_oblique_ping)
+            bs_normal.append(bs_normal_ping)
+            seabedimage_snippets.append(seabedimage_snippets_ping)
+            seabedimage_start.append(seabedimage_start_ping)
+            seabedimage_numsamples.append(seabedimage_numsamples_ping)
+            seabedimage_centersample.append(seabedimage_centersample_ping)
+
+    #Seabed Image Averaging
+    num_pings = len(bs_oblique)
+    num_beams = len(seabedimage_numsamples[0])
+    seabedimage = np.zeros((num_pings, num_beams))
+    seabedimage[:] = np.nan
+    seabedimage_center = np.zeros((num_pings, num_beams))
+    seabedimage_center[:] = np.nan
+    for p in range(num_pings):
+        for b in range(num_beams):
+            snippet_beam = seabedimage_snippets[p][seabedimage_start[p][b]:seabedimage_start[p][b]+seabedimage_numsamples[p][b]]
+            snippet_beam = np.array(snippet_beam)/10
+            reflectivity_sb_beam = 10*np.log10(1/seabedimage_numsamples[p][b]*np.sum(10**(snippet_beam/10)))
+            seabedimage[p,b] = reflectivity_sb_beam
+            seabedimage_center[p,b] = seabedimage_snippets[p][seabedimage_centersample[p][b]]
+
+
 
     data['ping']['reflectivity1'] = reflectivity1
 
-    # reflectivity1 = data['ping']['reflectivity1'] #added this datagram
-    # reflectivity = reflectivity1
+    reflectivity = reflectivity1
 
     beampointingangle = data['ping']['beampointingangle']
     beam_angle_bins = np.arange(-80,81,1)*(-1)
@@ -365,8 +401,7 @@ def prepare_backscatter_for_file(fle):
     lc_pings[:,:] = np.nan
     crossover_angle_runtime = float(data['runtime_params']['runtime_settings'][0]['Normal incidence corr.']) #added this datagram
 
-
-    reflectivity_improved = reflectivity - tvg_rt + tvg_improved
+    reflectivity_improved = reflectivity - tvg + tvg_improved
 
     # bs_o_minus_bs_n_list = []
     bs_n_list = []
@@ -401,7 +436,6 @@ def prepare_backscatter_for_file(fle):
 
 
 
-
     # If NNIC in TVG is flipped...
     # TVGkmall should be TVG = NNIC + LC - EAC + 2TL
     # If sign is flipped: TVG = -NNIC + LC - EAC + 2TL
@@ -411,16 +445,14 @@ def prepare_backscatter_for_file(fle):
     # backscatter = reflectivity - tvg - eac + tl2 + 2*nnic_pings
     # The above line makes for a coherent picture, but I don't think it actually makes a difference for the cal.
 
-    backscatter = reflectivity - tvg - eac + tl2
+    # backscatter = reflectivity - tvg - eac + tl2
+
+    reflectivity_final = reflectivity_improved - nnic_pings - lc_pings
 
     reflectivity_binned = np.empty((num_pings, len(beam_angle_bins)))
     reflectivity_binned[:,:] = np.nan
-    reflectivity1_binned = np.empty((num_pings, len(beam_angle_bins)))
-    reflectivity1_binned[:,:] = np.nan
     sectors_binned = np.empty((num_pings, len(beam_angle_bins)))
     sectors_binned[:,:] = np.nan
-    backscatter_binned = np.empty((num_pings, len(beam_angle_bins)))
-    backscatter_binned[:,:] = np.nan
     r_binned = np.empty((num_pings, len(beam_angle_bins)))
     r_binned[:,:] = np.nan
 
@@ -429,50 +461,144 @@ def prepare_backscatter_for_file(fle):
             sectors[n] = sectors[n] + 3*np.ones(len(sectors[n]))
 
         beam_angle_ping = beampointingangle[n]
-        reflectivity_ping = reflectivity[n] #THIS IS prob WRONG removing real-time tvg, adding spreading loss, absopriton loss (assuing units are db/km)
+        reflectivity_ping = reflectivity_final[n]
         bin_ind1 = np.nonzero(np.nanmax(beam_angle_ping) > beam_angle_bins)[0][1]
         bin_ind2 = np.nonzero(np.nanmin(beam_angle_ping) > beam_angle_bins)[0][0]
         reflectivity_binned[n, bin_ind1:bin_ind2] = interp1d(beam_angle_ping, reflectivity_ping)(beam_angle_bins[bin_ind1:bin_ind2])
-        backscatter_binned[n, bin_ind1:bin_ind2] = interp1d(beam_angle_ping, backscatter[n])(beam_angle_bins[bin_ind1:bin_ind2])
         sectors_binned[n, bin_ind1:bin_ind2] = np.round(interp1d(beam_angle_ping, sectors[n])(beam_angle_bins[bin_ind1:bin_ind2]))
         r_binned[n, bin_ind1:bin_ind2] = interp1d(beam_angle_ping, r[n])(beam_angle_bins[bin_ind1:bin_ind2])
 
     r_binned_mean = np.mean(r_binned, axis=0)
-    backscatter_mean = np.mean(backscatter_binned, axis=0)
+    reflectivity_mean = np.mean(reflectivity_binned, axis=0)
     sector_list = np.unique(sectors_binned)
     sector_list = sector_list[np.isnan(sector_list)==False]
     num_sectors = int(np.nanmax(sector_list)+1)
-    num_pings = np.shape(backscatter_binned)[0]
-    num_bins = np.shape(backscatter_binned)[1]
-    backscatter_sectorized = np.empty((num_sectors, num_pings, num_bins))
-    backscatter_sectorized[:, :, :] = np.nan
+    num_pings = np.shape(reflectivity_binned)[0]
+    num_bins = np.shape(reflectivity_binned)[1]
+    reflectivity_sectorized = np.empty((num_sectors, num_pings, num_bins))
+    reflectivity_sectorized[:, :, :] = np.nan
 
     for sector in range(num_sectors):
             for ping in range(num_pings):
                 for bin in range(num_bins):
                     if sectors_binned[ping, bin] == sector:
-                        backscatter_sectorized[sector, ping, bin] = backscatter_binned[ping, bin]
+                        reflectivity_sectorized[sector, ping, bin] = reflectivity_binned[ping, bin]
                     else:
-                        backscatter_sectorized[sector, ping, bin] = np.nan
+                        reflectivity_sectorized[sector, ping, bin] = np.nan
 
-    results = [{'mode': mode}, {'backscatter_binned': backscatter_binned},
-                         {'backscatter_sectorized': backscatter_sectorized},
-                         {'sectors_binned': sectors_binned}, {'r_binned_mean': r_binned_mean},
-                         {'beam_angle_bins': beam_angle_bins}]
+    results = {'mode': mode,
+               'reflectivity_binned': reflectivity_binned,
+               'reflectivity_sectorized': reflectivity_sectorized,
+               'sectors_binned': sectors_binned,
+               'r_binned_mean': r_binned_mean,
+               'beam_angle_bins': beam_angle_bins}
 
     return data, results
 
 
-def package_backscatter_from_fles(fles):
+def package_backscatter_from_dir(fle_dir):
+    fles = glob.glob(fle_dir+'\\*.kmall')
+    print(str(len(fles)) + ' files to process.')
     data_dict = {}
     results_dict = {}
 
     for fle in fles:
-        data, results = prepare_backscatter_for_file(fles)
+        print('Processing ' + fle)
+        data, results = prepare_backscatter_for_file(fle)
         data_dict[fle] = data
         results_dict[fle] = results
 
     return data_dict, results_dict
+
+
+def backscatter_correction(results_dict):
+    fles = list(results_dict.keys())
+    modes = []
+    bs_scalar_means = []
+    beam_angle_bins = results_dict[fles[0]]['beam_angle_bins']
+
+    #Determining all the modes in the directory
+    for fle in fles:
+        mode = results_dict[fle]['mode']
+        bs_scalar_means.append(np.nanmean(results_dict[fle]['reflectivity_binned']))
+        modes.append(mode)
+    modes = list(set(modes))
+
+    bs_scalar_mean = np.mean(bs_scalar_means)
+    #populating a dictionary with key equal to mode and value equal to associated files
+    mode_dict = {}
+    for mode in modes:
+        fle_mode = []
+        for fle in fles:
+            if results_dict[fle]['mode'] == mode:
+                fle_mode.append(fle)
+        mode_dict[mode] = {'files': fle_mode}
+
+    for mode in modes:
+        fles = mode_dict[mode]['files']
+        if len(fles) != 2:
+            print('Found '+ str(len(fles)) + ' files. Did not find 2 files for ' + mode + ' mode. Cannot complete calibration. Check directory.')
+
+        bs_1 = results_dict[fles[0]]['reflectivity_sectorized']
+        bs_2 = results_dict[fles[1]]['reflectivity_sectorized']
+        bs_1_mean = np.nanmean(bs_1, axis=1)
+        bs_2_mean = np.nanmean(bs_2, axis=1)
+        bs_mean = (bs_1_mean+bs_2_mean)/2
+        bs_mean_corr = bs_mean #corrected mean backscatter for a given mode as a function of transducer relative angle
+        bs_corr = bs_mean_corr - bs_scalar_mean
+        mode_dict[mode]['bs_corr'] = bs_corr
+        mode_dict[mode]['beam_angle_bins'] = beam_angle_bins
+
+    return mode_dict
+
+def make_calib_file(mode_dict, nadir_mask_angle):
+    port_sector_angles = np.flipud(np.arange(30,81,1))
+    center_sector_angles = np.flipud(np.arange(-50,51,1))
+    stbd_sector_angles = np.flipud(np.arange(-80,-29,1))
+
+
+    calib_angles = {'Very shallow': [np.flipud(np.arange(30,81,1)), np.flipud(np.arange(-50,51,1)), np.flipud(np.arange(-80,-29,1))],
+              'Shallow': [np.flipud(np.arange(30,81,1)), np.flipud(np.arange(-50,51,1)), np.flipud(np.arange(-80,-29,1))],
+              'Medium': [np.flipud(np.arange(30,81,1)), np.flipud(np.arange(-50,51,1)), np.flipud(np.arange(-80,-29,1))],
+              'Deep': [np.flipud(np.arange(30, 81, 1)), np.flipud(np.arange(-50, 51, 1)), np.flipud(np.arange(-80, -29, 1))],
+              'Very deep': [np.flipud(np.arange(20, 71, 1)), np.flipud(np.arange(-40, 41, 1)), np.flipud(np.arange(-70, -19, 1))],
+              'Extra deep': [np.flipud(np.arange(20, 61, 1)), np.flipud(np.arange(-40, 41, 1)), np.flipud(np.arange(-60, -19, 1))]
+              }
+
+    modes = mode_dict.keys()
+
+    calib_file_dict = {}
+
+    for mode in modes:
+        calib_vals = mode_dict[mode]['bs_corr']*(-1) #Flipping per JHC advice
+        sector_num = calib_vals.shape[0]
+        calib_file_dict[mode] = {}
+
+        for sector in range(sector_num):
+            angle_bins = mode_dict[mode]['beam_angle_bins']
+            bs_corr = mode_dict[mode]['bs_corr'][sector]
+            bs_corr = bs_corr.round(2)
+            calib_angles_sector = calib_angles[mode][int(np.remainder(sector,sector_num/2))]
+            # bs_corr_sector = np.interp(calib_angles_sector, angle_bins, bs_corr)
+            bs_corr_sector = np.ones(len(calib_angles_sector))
+            bs_corr_sector[:] = np.nan
+            for n in range(len(calib_angles_sector)):
+                bs_corr_sector[n] = bs_corr[np.argwhere(angle_bins == calib_angles_sector[n])]
+            if True in np.isnan(bs_corr_sector):
+                ind1 = np.argwhere(np.isnan(bs_corr_sector) == False)[0][0]
+                ind2 = np.argwhere(np.isnan(bs_corr_sector) == False)[-1][0]
+                bs_corr_sector[:ind1] = bs_corr_sector[ind1+1]
+                bs_corr_sector[ind2+1:] = bs_corr_sector[ind2]
+            if 0 in calib_angles_sector:
+                ind1 = np.argwhere(calib_angles_sector == nadir_mask_angle)[0][0]
+                ind2 = np.argwhere(calib_angles_sector == -nadir_mask_angle)[0][0]
+                bs_corr_sector[ind1:ind2] = np.interp(calib_angles_sector[ind1:ind2],
+                                                      [calib_angles_sector[ind2], calib_angles_sector[ind1]],
+                                                      [bs_corr_sector[ind2], bs_corr_sector[ind1]])
+
+            calib_file_dict[mode][sector] = {'calib_angles': calib_angles_sector, 'bs_corr': bs_corr_sector}
+
+    return calib_file_dict
 
 def lc_nnic_improved(results_dict, crossover_angle):
     fles = list(results_dict.keys())
@@ -522,43 +648,6 @@ def lc_nnic_improved(results_dict, crossover_angle):
 
     return lc_nnic, bs_scalar_mean
 
-def backscatter_correction(results_dict, lc_nnic, bs_scalar_mean):
-    fles = list(results_dict.keys())
-    modes = []
-    beam_angle_bins = results_dict[fles[0]][5]['beam_angle_bins']
-
-    #Determining all the modes in the directory
-    for fle in fles:
-        mode = results_dict[fle][0]['mode']
-        modes.append(mode)
-    modes = list(set(modes))
-
-    #populating a dictionary with key equal to mode and value equal to associated files
-    mode_dict = {}
-    for mode in modes:
-        fle_mode = []
-        for fle in fles:
-            if results_dict[fle][0]['mode'] == mode:
-                fle_mode.append(fle)
-        mode_dict[mode] = {'files': fle_mode}
-
-    for mode in modes:
-        fles = mode_dict[mode]['files']
-        if len(fles) != 2:
-            print('Did not find 2 files for ' + mode + ' mode. Cannot complete calibration. Check directory.')
-
-        bs_1 = results_dict[fles[0]][2]['backscatter_sectorized']
-        bs_2 = results_dict[fles[1]][2]['backscatter_sectorized']
-        bs_1_mean = np.nanmean(bs_1, axis=1)
-        bs_2_mean = np.nanmean(bs_2, axis=1)
-        bs_mean = (bs_1_mean+bs_2_mean)/2
-        bs_mean_corr = bs_mean - lc_nnic #corrected mean backscatter for a given mode as a function of transducer relative angle
-        bs_corr = bs_mean_corr - bs_scalar_mean
-        mode_dict[mode]['bs_corr'] = bs_corr
-        mode_dict[mode]['beam_angle_bins'] = beam_angle_bins
-
-    return mode_dict
-
 def plot_curves(mode_dict):
     modes = list(mode_dict.keys())
     plt.ioff()
@@ -567,14 +656,31 @@ def plot_curves(mode_dict):
         mode = modes[n]
         bs_corr = mode_dict[mode]['bs_corr']
         beam_angle_bins = mode_dict[mode]['beam_angle_bins']
-        for m in range(len(np.shape(bs_corr)[0])):
-            ax[n].plot(beam_angle_bins, bs_corr[m,:])
+        for m in range(np.shape(bs_corr)[0]):
+            ax[n].plot(-beam_angle_bins, bs_corr[m,:]) #Beam angle bins are negative because kongsberg indexes backwards. Port side is positive, stbd side is negative.
+    plt.show()
 
+def plot_calib_file(calib_file_dict):
+    modes = list(calib_file_dict.keys())
+    plt.ioff()
+    fig, ax = plt.subplots(len(modes),1)
+    for n in range(len(modes)):
+        mode = modes[n]
+        sectors = calib_file_dict[mode].keys()
+        for sector in sectors:
+            ax[n].plot(calib_file_dict[mode][sector]['calib_angles'], calib_file_dict[mode][sector]['bs_corr'])
+    plt.show()
 
 if __name__ == "__main__":
 
     #Example of running bs_evaluation on a directory with .kmall data.
-    fle_dir = r'D:\Backscatter\OPR-T381-RA-22_Mariana_Islands\H13578\MBES\S221_Simrad-EM710_ICE\2022-140'
-    create_raw_bs_evaluation(fle_dir)
+    # fle_dir = r'D:\Backscatter\OPR-O392-FA-23\H13776'
+    # create_raw_bs_evaluation(fle_dir)
+
+    fle = r"D:\Fairweather\FA_2023_Backscatter_Calibration\100m Data Test 1\MBES\0002_20230404_101731.kmall"
+    # data, results = prepare_backscatter_for_file(fle)
+    fle_dir = r'D:\Fairweather\FA_2023_Backscatter_Calibration\100m Data Test 1\MBES\Test'
+    data_dict, results_dict = package_backscatter_from_dir(fle_dir)
+    mode_dir = backscatter_correction(results_dict)
 
     print('Complete')
