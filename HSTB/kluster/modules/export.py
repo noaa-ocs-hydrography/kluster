5825a6e7ff5bda8b63d5694f426e88a6fa75b54c
Harper Umfress <harper.umfress@gmail.com>
2025-05-05 12:17:50 -0700

    Also handing case of '-' in line name, which doesnt work in ESRI

--- HSTB/kluster/modules/export.py
+++ HSTB/kluster/modules/export.py
@@ -251,7 +251,9 @@ class FqprExport:
                                                  self.fqpr.multibeam.raw_ping[0].multibeam_files[line][1]
                 nav = self.fqpr.return_navigation(line_start_time, line_end_time)
                 if nav is not None:
+                    # '.' and '_' don't work in ESRI.
                     line_layer = line.replace('.','_')
+                    line_layer = line_layer.replace('-', '_') 
                     vl.write_to_layer(line_layer, np.column_stack([nav.longitude.values, nav.latitude.values]), 2)  # ogr.wkbLineString
                 else:
                     print(f'export_lines_to_geopackage: unable to access raw navigation for line {line}')
